40912825b1afc1fe6831b0692091b36db0a16809
AlejandroCRios <al.cardonarios@gmail.com>
2021-05-20 21:08:27 -0500

    index page creada y funcionando

--- proyectoSENA/contacto/views.py
+++ proyectoSENA/contacto/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import Contact #forms.py
 from django.core.mail import EmailMessage
 
-#this feature receive data from HTML and sent it through email, it isn't set for security
+
 def contacto(request):
     #importa forms que a su vez impotan models
     contact = Contact()
@@ -10,16 +10,19 @@ def contacto(request):
     if request.method == 'POST':
         contact = Contact(data = request.POST)
         if contact.is_valid():
+            
+            #reciben los datos que el usuario ingresa al formulario
             nombre = request.POST.get("name")
             email = request.POST.get("email")
             phone = request.POST.get("phone")
             message = request.POST.get("message")
             
             
-            email = EmailMessage("Mensaje de AdoptApp", "The user with name{} with the email {} and phone{} ,writes following: " .format(nombre,email,phone,message), 
+            email = EmailMessage("Mensaje de AdoptApp", "El usuario con el nombre {} con el email {} y el telefono {}, escribre los siguiente: {} " .format(nombre,email,phone,message), 
                                  "",["newproyectosena2021@outlook.com"],reply_to=[email])
 
             try:
+                #Envia los datos al correo del proyecto
                 email.send()
                 return redirect("/contacto/?valid")
             except:
