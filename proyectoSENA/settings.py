20abe95050f6134c45b6d286058a777506fedf6c
AlejandroCRios <al.cardonarios@gmail.com>
2021-06-23 16:33:42 -0500

    Settings modified and working

--- proyectoSENA/settings.py
+++ proyectoSENA/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'ch*(p47@6101ca=@rvfr&#kd(xat!w0atb6bda==w!p759md^d'
+#SECRET_KEY = 'ch*(p47@6101ca=@rvfr&#kd(xat!w0atb6bda==w!p759md^d'
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY = 'ch*(p47@6101ca=@rvfr&#kd(xat!w0atb6bda==w!p759md^d')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ch*(p47@6101ca=@rvfr&#kd(xat!w0atb6bda==w!p759md^d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
